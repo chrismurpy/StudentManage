6142d4d293fcdca8e0afc85fa07dcc9cede0cb77
murphy <631341307@qq.com>
2021-05-18 20:58:30 +0800

    优化 - 用户权限判断

---
+++ src/com/murphy/filter/VisitFilter.java
@@ -0,0 +1,33 @@
+package com.murphy.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author murphy
+ */
+public class VisitFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        String requestURI = request.getRequestURI();
+        Object u1 = request.getSession().getAttribute("u1");
+        if (u1 == null && !requestURI.endsWith("/login.jsp")){
+            response.sendRedirect("login.jsp");
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
